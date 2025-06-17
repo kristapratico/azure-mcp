2871b9845aa92a990162d8ce02cde99de6152084
Krista Pratico <krpratic@microsoft.com>
2025-06-17 16:09:03 -0700

    rename + typing

---
+++ tests/Evals/README.md
@@ -0,0 +1,7 @@
+# Evals for azure-mcp
+
+This directory contains evaluation script for the Azure MCP project.
+
+
+
+
--- tests/Evals/run.py
+++ tests/Evals/run.py
@@ -2,15 +2,18 @@ import os
 import asyncio
 import pathlib
 import json
+from typing import Any
 
 # set before azure.ai.evaluation import to make PF output less noisy
 os.environ["PF_LOGGING_LEVEL"] = "CRITICAL"
 
 import dotenv
 from openai import AzureOpenAI
+from openai.types import chat
 from mcp.client.stdio import stdio_client
 from mcp import ClientSession, StdioServerParameters
-from azure.ai.evaluation import evaluate, ToolCallAccuracyEvaluator
+from mcp.types import CallToolResult
+from azure.ai.evaluation import evaluate, ToolCallAccuracyEvaluator, AzureAIProject
 from azure.identity import AzurePipelinesCredential, DefaultAzureCredential, get_bearer_token_provider
 
 
@@ -18,7 +21,7 @@ dotenv.load_dotenv()
 
 # for best results, model judge should always be a different model from the one we are evaluating
 MODEL = "gpt-4o"
-MODEL_JUDGE = "gpt-4o"
+MODEL_JUDGE = "o3-mini"
 API_VERSION = "2025-03-01-preview"
 
 model_config: dict[str, str] = {
@@ -28,63 +31,45 @@ model_config: dict[str, str] = {
     "api_version": API_VERSION,
 }
 
-token_provider = get_bearer_token_provider(
-    DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default"
-)
+token_provider = get_bearer_token_provider(DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default")
 client = AzureOpenAI(
-    azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
-    api_version=API_VERSION,
-    azure_ad_token_provider=token_provider
+    azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"], api_version=API_VERSION, azure_ad_token_provider=token_provider
 )
 
-server_params = StdioServerParameters(
-    command="npx",
-    args=["-y", "@azure/mcp@latest", "server", "start"],
-    env=None
-)
+server_params = StdioServerParameters(command="npx", args=["-y", "@azure/mcp@latest", "server", "start"], env=None)
 
-options = {
-    "subscription": "Azure SDK Developer Playground",
-    "resource_group": "my-resource-group",
-    "storage account": "aifoundrystorageshared",
-    "cosmos resource": "archagent-cosmos",
-    "database": "guidelines",
-    "appconfig": "appconfig",
-    "search resource": "mysearch",
-    "index": "mysvc",
-    "kusto cluster": "mycluster",
-    "kusto db name": "db1",
-    "storage container": "bloby",
-    "cluster uri": "https://mycluster.kusto.windows.net",
-    "account-name": "my-cosmos-account",
-    "sub id": "faa080af-c1d8-40ad-9cce-e1a450ca5b57",
-}
+followup_answers = pathlib.Path(__file__).parent / "followup.json"
+with open(str(followup_answers), "r") as f:
+    options = json.load(f)
 
 
 def in_ci() -> bool:
     return os.getenv("TF_BUILD") is not None
 
 
-def reshape_tools(tools) -> list[dict]:
-    return [{
-        "type": "tool_call",
-        "tool_call_id": tool.id,
-        "name": tool.function.name,
-        "arguments": json.loads(tool.function.arguments)
-    } for tool in tools]
+def reshape_tools(tools: list[chat.ChatCompletionMessageToolCall]) -> list[dict[str, Any]]:
+    return [
+        {
+            "type": "tool_call",
+            "tool_call_id": tool.id,
+            "name": tool.function.name,
+            "arguments": json.loads(tool.function.arguments),
+        }
+        for tool in tools
+    ]
 
 
-def reshape_tool_definitions(available_tools) -> list[dict]:
+def reshape_tool_definitions(available_tools: list[dict[str, Any]]) -> list[dict[str, Any]]:
     tools = [tool["function"] for tool in available_tools]
-    return [{
-        "name": tool["name"],
-        "description": tool["description"],
-        "parameters": tool["parameters"],
-        "type": "function"
-    } for tool in tools]
+    return [
+        {"name": tool["name"], "description": tool["description"], "parameters": tool["parameters"], "type": "function"}
+        for tool in tools
+    ]
 
 
-async def call_mcp_tool(tool_call, function_args):
+async def call_mcp_tool(
+    tool_call: chat.ChatCompletionMessageToolCall, function_args: dict[str, Any]
+) -> CallToolResult:
     async with stdio_client(server_params) as (read, write):
         async with ClientSession(read, write) as session:
             await session.initialize()
@@ -95,27 +80,30 @@ async def call_mcp_tool(tool_call, function_args):
     return result
 
 
-def make_request(response, messages, tools, tool_calls_made):
+def make_request(
+    messages: list[chat.ChatCompletionMessage],
+    available_tools: list[dict[str, Any]],
+    tool_calls_made: list[chat.ChatCompletionMessageToolCall],
+) -> tuple[list[chat.ChatCompletionMessage], list[chat.ChatCompletionMessageToolCall]]:
     answer = None
+    response = messages[-1]
     while answer is None:
         tool_messages = []
         for tool_call in response.tool_calls:
             function_args = json.loads(tool_call.function.arguments)
             result = asyncio.run(call_mcp_tool(tool_call, function_args))
 
-            tool_messages.append({
-                "tool_call_id": tool_call.id,
-                "role": "tool",
-                "name": tool_call.function.name,
-                "content": result.content[0].text,
-            })
+            tool_messages.append(
+                {
+                    "tool_call_id": tool_call.id,
+                    "role": "tool",
+                    "name": tool_call.function.name,
+                    "content": result.content[0].text,
+                }
+            )
         messages.extend(tool_messages)
 
-        response = client.chat.completions.create(
-            model=MODEL,
-            messages=messages,
-            tools=tools
-        )
+        response = client.chat.completions.create(model=MODEL, messages=messages, tools=available_tools)
         if response.choices[0].message.tool_calls:
             tool_calls_made.extend(response.choices[0].message.tool_calls)
         answer = response.choices[0].message.content
@@ -126,18 +114,16 @@ def make_request(response, messages, tools, tool_calls_made):
         response = response.choices[0].message
 
 
-def evaluate_azure_mcp(query: str, expected_tool_calls: dict):
+def evaluate_azure_mcp(query: str, expected_tool_calls: list):
     messages = [{"role": "user", "content": query}]
     available_tools = asyncio.run(get_tools())
 
     attempts = 0
     while True:
         attempts += 1
-        response = client.chat.completions.create(
-            model=MODEL,
-            messages=messages,
-            tools=available_tools
-        ).choices[0].message
+        response = (
+            client.chat.completions.create(model=MODEL, messages=messages, tools=available_tools).choices[0].message
+        )
 
         messages.append(response)
 
@@ -150,18 +136,24 @@ def evaluate_azure_mcp(query: str, expected_tool_calls: dict):
             f"You are the user. Given the following options: {json.dumps(options)}, "
             f"answer the assistant's question as concisely as possible: {followup_question}"
         )
-        response = client.chat.completions.create(
-            model=MODEL,
-            messages=[{"role": "system", "content": "You are a helpful user."},
-                      {"role": "user", "content": user_prompt}]
-        ).choices[0].message
+        response = (
+            client.chat.completions.create(
+                model=MODEL,
+                messages=[
+                    {"role": "system", "content": "You are a helpful user."},
+                    {"role": "user", "content": user_prompt},
+                ],
+            )
+            .choices[0]
+            .message
+        )
 
         messages.append({"role": "user", "content": response.content})
 
     tool_calls_made = []
     if response.tool_calls is not None:
         tool_calls_made.extend(response.tool_calls)
-        messages, tool_calls = make_request(response, messages, available_tools, tool_calls_made)
+        messages, tool_calls = make_request(messages, available_tools, tool_calls_made)
         tool_calls_made = reshape_tools(tool_calls)
 
     tool_defs = reshape_tool_definitions(available_tools)
@@ -173,34 +165,31 @@ def evaluate_azure_mcp(query: str, expected_tool_calls: dict):
         "tool_definitions": tool_defs,
         "expected": expected_tool_calls,
         "called_expected": any(
-            actual["name"] == expected
-            for actual in tool_calls_made
-            for expected in expected_tool_calls
+            actual["name"] == expected for actual in tool_calls_made for expected in expected_tool_calls
         ),
         "num_tool_calls_actual": len(tool_calls_made),
         "num_tool_calls_expected": len(expected_tool_calls),
     }
 
 
-async def get_tools():
+async def get_tools() -> list[dict[str, Any]]:
     async with stdio_client(server_params) as (read, write):
         async with ClientSession(read, write) as session:
             await session.initialize()
 
             tools = await session.list_tools()
-            available_tools = [{
-                "type": "function",
-                "function": {
-                    "name": tool.name,
-                    "description": tool.description,
-                    "parameters": tool.inputSchema
+            available_tools = [
+                {
+                    "type": "function",
+                    "function": {"name": tool.name, "description": tool.description, "parameters": tool.inputSchema},
                 }
-            } for tool in tools.tools]
+                for tool in tools.tools
+            ]
     return available_tools
 
 
 if __name__ == "__main__":
-    azure_ai_project = {
+    azure_ai_project: AzureAIProject = {
         "subscription_id": os.environ["AZURE_SUBSCRIPTION_ID"],
         "resource_group_name": os.environ["AZURE_FOUNDRY_RESOURCE_GROUP"],
         "project_name": os.environ["AZURE_FOUNDRY_PROJECT_NAME"],
@@ -221,9 +210,9 @@ if __name__ == "__main__":
     else:
         kwargs = {}
 
-    tool_call_accuracy = ToolCallAccuracyEvaluator(model_config=model_config)
+    tool_call_accuracy = ToolCallAccuracyEvaluator(model_config=model_config, is_reasoning_model=True)
 
-    test_file = pathlib.Path(__file__).parent / "test.jsonl"
+    test_file = pathlib.Path(__file__).parent / "data.jsonl"
     result = evaluate(
         data=str(test_file),
         evaluators={
@@ -231,5 +220,6 @@ if __name__ == "__main__":
         },
         target=evaluate_azure_mcp,
         azure_ai_project=azure_ai_project,
-        **kwargs
+        fail_on_evaluator_errors=True,
+        **kwargs,
     )
